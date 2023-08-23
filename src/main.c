6783b985947af38b41516c00bd54ebd2df021d8a
pedrofracassi <pedroluizfracassi@gmail.com>
2023-08-23 20:33:57 -0300

    Botões controlando LEDs

--- src/main.c
+++ src/main.c
@@ -23,10 +23,35 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED1_PIO           PIOA
+#define LED1_PIO_ID        ID_PIOA
+#define LED1_PIO_IDX       0
+#define LED1_PIO_IDX_MASK  (1 << LED1_PIO_IDX)
+
+#define LED2_PIO           PIOC
+#define LED2_PIO_ID        ID_PIOC
+#define LED2_PIO_IDX       30
+#define LED2_PIO_IDX_MASK  (1 << LED2_PIO_IDX)
+
+#define LED3_PIO           PIOB
+#define LED3_PIO_ID        ID_PIOB
+#define LED3_PIO_IDX       2
+#define LED3_PIO_IDX_MASK  (1 << LED3_PIO_IDX)
+
+#define BUT1_PIO           PIOD
+#define BUT1_PIO_ID        ID_PIOD
+#define BUT1_PIO_IDX       28
+#define BUT1_PIO_IDX_MASK  (1 << BUT1_PIO_IDX)
+
+#define BUT2_PIO           PIOC
+#define BUT2_PIO_ID        ID_PIOC
+#define BUT2_PIO_IDX       31
+#define BUT2_PIO_IDX_MASK  (1 << BUT2_PIO_IDX)
+
+#define BUT3_PIO           PIOA
+#define BUT3_PIO_ID        ID_PIOA
+#define BUT3_PIO_IDX       19
+#define BUT3_PIO_IDX_MASK  (1 << BUT3_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,7 +78,34 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
-
+	sysclk_init();
+	
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+	
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+	
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+	
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+	
+	pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+	pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+	pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
 }
 
 /************************************************************************/
@@ -69,7 +121,39 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
-
+	  int but1_val = pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK);
+	  
+	  if (but1_val == 0) {
+		  for (int i = 0; i < 5; i++) {
+			pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+			delay_ms(200);
+			pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+			delay_ms(200);
+		  }
+	  }
+	  
+	  int but2_val = pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK);
+	  
+	  if (but2_val == 0) {
+		  for (int i = 0; i < 5; i++) {
+			pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+			delay_ms(200);
+			pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+			delay_ms(200);
+		  }
+	  }
+	  
+	  int but3_val = pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK);
+	  
+	  if (but3_val == 0) {
+		  for (int i = 0; i < 5; i++) {
+			pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+			delay_ms(200);
+			pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+			delay_ms(200);
+		  }
+	  }
+	
   }
   return 0;
 }
